90dcb3fc6d3a3149f3454970071c2b8989320b1f
Saurabh-crypto16 <prakashsaurabh161228@gmail.com>
2022-08-07 15:37:29 +0530

    added delete,update and query update features

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports={
-    MONGOURI:"mongodb://localhost/skrate",
-    JWT_SECRET:"jkjknjksdkjsdcklmdslkwe"
-}
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://saurabh161228:saurabh161228@cluster0.4vh7ze2.mongodb.net/?retryWrites=true&w=majority",
+  JWT_SECRET: "jkjknjksdkjsdcklmdslkwe",
+};
--- routes/auth.js
+++ routes/auth.js
@@ -14,12 +14,12 @@ router.get("/protected", requireLogin, (req, res) => {
 });
 
 // To signup a new user and return bearer token
-router.post("/users/new", (req, res) => {
+router.post("/users/new", async (req, res) => {
   const { username, role } = req.body;
   if (!username || !role) {
     return res.status(422).json({ error: "Please fill all the fields" });
   }
-  User.findOne({ username: username }).then((savedUser) => {
+  await User.findOne({ username: username }).then((savedUser) => {
     if (savedUser) {
       return res
         .status(422)
@@ -43,14 +43,14 @@ router.post("/users/new", (req, res) => {
 });
 
 // add new ticket
-router.post("/tickets/new", requireLogin, (req, res) => {
+router.post("/tickets/new", requireLogin, async (req, res) => {
   const { title, description } = req.body;
   const data = new Ticket({
     _id: uuid.v4(),
     title,
     description,
   });
-  data
+  await data
     .save()
     .then((result) => {
       res.json({ details: result._id });
@@ -61,8 +61,8 @@ router.post("/tickets/new", requireLogin, (req, res) => {
 });
 
 //show all tickets
-router.get("/tickets/all", requireLogin, (req, res) => {
-  Ticket.find()
+router.get("/tickets/all", requireLogin, async (req, res) => {
+  await Ticket.find()
     .populate(
       "detailOf",
       "_id title description status priority assignedTo createdAt"
@@ -75,11 +75,58 @@ router.get("/tickets/all", requireLogin, (req, res) => {
     });
 });
 
+//show query tickets
+router.get("/tickets/", requireLogin, async (req, res) => {
+  const status = req.query.status;
+  const title = req.query.title;
+  const priority = req.query.priority;
+
+  if (status) {
+    await Ticket.find({ status: status })
+      .populate(
+        "detailOf",
+        "_id title description status priority assignedTo createdAt"
+      )
+      .then((details) => {
+        res.json({ details });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else if (title) {
+    await Ticket.find({ title: title })
+      .populate(
+        "detailOf",
+        "_id title description status priority assignedTo createdAt"
+      )
+      .then((details) => {
+        res.json({ details });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else if (priority) {
+    await Ticket.find({ priority: priority })
+      .populate(
+        "detailOf",
+        "_id title description status priority assignedTo createdAt"
+      )
+      .then((details) => {
+        res.json({ details });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    res.json({ status: error });
+  }
+});
+
 //delete ticket
-router.get("/tickets/delete", requireLogin, (req, res) => {
+router.post("/tickets/delete", requireLogin, async (req, res) => {
   const { user, ticketId } = req.body;
   if (user.role === "admin") {
-    Ticket.findByIdAndDelete(ticketId)
+    await Ticket.findByIdAndDelete(ticketId)
       .then((result) => {
         res.json({ deletedTicket: ticketId });
       })
@@ -88,6 +135,41 @@ router.get("/tickets/delete", requireLogin, (req, res) => {
       });
   } else {
     res.json({ error: "Only admin can delete a ticket" });
+  }
+});
+
+//close ticket
+router.post("/tickets/markAsClosed", requireLogin, async (req, res) => {
+  const { user, ticketId } = req.body;
+
+  const ticket = await Ticket.findById(ticketId).catch((err) => {
+    console.log(err);
+  });
+
+  if (ticket) {
+    if (user.role === "admin" || ticket.assignedTo == user.username) {
+      await Ticket.find({ priority: priority })
+        .populate(
+          "detailOf",
+          "_id title description status priority assignedTo createdAt"
+        )
+        .then((details) => {
+          //check higher priority
+          if (ticket.priority == "low") {
+            //check if any medium or higher priority exists
+          } else if (ticket.priority == "high") {
+            //check if any medium priority exists
+          } else {
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      res.json({ error: "Only admin or assigned user can update a ticket" });
+    }
+  } else {
+    res.json({ error: "Ticket does not exist" });
   }
 });
 
